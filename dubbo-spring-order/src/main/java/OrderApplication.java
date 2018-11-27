2e4ac06a1ad878326724b1dfff2b71df13bcbf7e
duanzx <15726645490@163.com>
2018-11-27 14:55:44 +0800

    项目初始化

---
+++ dubbo-spring-order/src/main/java/OrderApplication.java
@@ -0,0 +1,5 @@
+public class OrderApplication {
+    public static void main(String[] args)throws Exception{
+
+    }
+}
---
+++ dubbo-spring-user/src/main/java/UserApplication.java
@@ -0,0 +1,6 @@
+public class UserApplication {
+
+    public static void main(String[] args)throws Exception{
+
+    }
+}
