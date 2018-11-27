377a93cf334fa2ddcf958123d92e5ef70ff3e51a
duanzx <15726645490@163.com>
2018-11-27 15:27:32 +0800

    项目初始化-添加业务实现

---
+++ dubbo-spring-user/src/main/java/cn/duanzx/user/service/UserService.java
@@ -0,0 +1,10 @@
+package cn.duanzx.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void printOrderInfo(String orderId) {
+    }
+}
