be87a58644adf6019e48c2b09f666a9204b411d7
duanzx <15726645490@163.com>
2018-11-27 16:16:30 +0800

    项目初始化-添加dubbo支持，并配置dubbo

--- dubbo-spring-order/src/main/java/cn/duanzx/order/configuration/DubboConfiguration.java
+++ dubbo-spring-order/src/main/java/cn/duanzx/order/configuration/DubboConfiguration.java
@@ -1,9 +1,11 @@
 package cn.duanzx.order.configuration;
 
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDubboConfig
+@DubboComponentScan(basePackages = "cn.duanzx.user.controller")
 public class DubboConfiguration {
 }
--- dubbo-spring-user/src/main/java/cn/duanzx/user/configuration/DubboConfiguration.java
+++ dubbo-spring-user/src/main/java/cn/duanzx/user/configuration/DubboConfiguration.java
@@ -1,9 +1,11 @@
 package cn.duanzx.user.configuration;
 
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDubboConfig
+@DubboComponentScan(basePackages = "cn.duanzx.order.service")
 public class DubboConfiguration {
 }
