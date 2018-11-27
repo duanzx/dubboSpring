29f343abaf693bd0f53392b36c8f34c50c86186c
duanzx <15726645490@163.com>
2018-11-27 15:52:19 +0800

    项目初始化-添加dubbo支持，并配置dubbo

--- dubbo-spring-order/src/main/java/cn/duanzx/order/service/OrderService.java
+++ dubbo-spring-order/src/main/java/cn/duanzx/order/service/OrderService.java
@@ -3,12 +3,14 @@ package cn.duanzx.order.service;
 import cn.duanzx.module.bean.Order;
 import cn.duanzx.module.service.IOrderService;
 import cn.duanzx.order.repo.OrderRepo;
+import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Service
+@Service(interfaceClass = IOrderService.class, version = "1.0.0")
+@Component
 public class OrderService implements IOrderService {
     @Autowired
     private OrderRepo orderRepo;
